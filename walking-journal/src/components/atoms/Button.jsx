be0736b3e78aa64b29b4508c6d10c30fa52886c6
alermlv <102925147+alermlv@users.noreply.github.com>
2022-09-13 21:44:16 +0300

    Delete walking-journal directory

--- walking-journal/src/components/atoms/Button.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Button({ className, addToList, innerText }) {
-  return <button className={className} onClick={addToList}>{innerText}</button>
-}
--- walking-journal/src/components/atoms/ListHeaderItem.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ListHeaderItem({ innerText }) {
-  return <span>{innerText}</span>
-}
