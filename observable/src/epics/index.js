e3f8819c9007504fe6bca44857410bdbb141a70d
alermlv <102925147+alermlv@users.noreply.github.com>
2022-11-10 07:11:56 +0300

    Add files via upload

---
+++ observable/src/epics/index.js
@@ -0,0 +1,45 @@
+import { ofType } from "redux-observable";
+import { of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import {
+  catchError,
+  debounceTime,
+  filter,
+  map,
+  retry,
+  switchMap,
+} from "rxjs/operators";
+import {
+  CHANGE_SEARCH_FIELD,
+  SEARCH_SKILLS_REQUEST,
+} from "../actions/actionTypes";
+import {
+  searchSkillsRequest,
+  searchSkillsSuccess,
+  searchSkillsFailure,
+} from "../actions/actionCreators";
+
+export const changeSearchEpic = (action$) =>
+  action$.pipe(
+    ofType(CHANGE_SEARCH_FIELD),
+    map((o) => o.payload.search.trim()),
+    filter((o) => o !== ""),
+    debounceTime(100),
+    map((o) => searchSkillsRequest(o))
+  );
+
+const SEARCH_URL = "http://localhost:7070/api/items/search";
+
+export const searchSkillsEpic = (action$) =>
+  action$.pipe(
+    ofType(SEARCH_SKILLS_REQUEST),
+    map((o) => o.payload.search),
+    map((o) => new URLSearchParams({ q: 0 })),
+    switchMap((o) =>
+      ajax.getJSON(`${SEARCH_URL}?${o}`).pipe(
+        retry(3),
+        map((o) => searchSkillsSuccess(o)),
+        catchError((e) => of(searchSkillsFailure(e)))
+      )
+    )
+  );
